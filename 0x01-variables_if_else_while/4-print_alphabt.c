618fee1b306dacc5feb4673e474b57048a0c2d4e
MuhamaMark <muhamammark@gmail.com>
2022-07-02 14:22:14 -0700

    lower case minus q nad e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - entry point of a program
+ * Return: always 0 success
+ * exit is end of program
+ */
+int main(void)
+{
+	char lo = 'a';
+
+	while ( lo <= 'z' && (!= 'q' || != 'e'))
+	{
+		putchar(lo);
+		lo = lo + 1;
+	}
+	putchar('\n');
+	exit(EXIT_SUCCESS)
+}
