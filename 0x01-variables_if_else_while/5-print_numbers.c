e117abc3d02a3d7ad01703c497afe947a0c74086
MuhamaMark <muhamammark@gmail.com>
2022-07-02 16:03:45 -0700

    printing base 10 number from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry point of programme
+ * Return: always(0) success
+ */
+int main(void)
+{
+	int num = 0;
+
+	while(num <= 9)
+	{
+		printf("%d", num);
+		num = num + 1;
+	}
+	printf("\n");
+	return(0);
+}
