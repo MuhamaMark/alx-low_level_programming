55ed45011cf73a01b0a18d5b5b753ecee9178b53
MuhamaMark <muhamammark@gmail.com>
2022-07-02 15:18:21 -0700

    base 10 numbers fro 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - entry of program
+ * Return: alwasy 0 success
+ */
+int main()
+{
+	int num;
+	
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d",num);
+	}
+	printf("\n");
+	Return(0);
+}
