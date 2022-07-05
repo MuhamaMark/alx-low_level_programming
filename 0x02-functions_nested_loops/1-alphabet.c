ecd3a2a9d6fdbedd5f80f09352c2c34c0d0c13ab
MuhamaMark <muhamammark@gmail.com>
2022-07-05 14:05:57 -0700

    alphabet lower case using _putchar  function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point of the program
+ * Result: alwasy 0 success
+ * print lower case alphabet using headercreated
+ */
+int main(void)
+{
+	char lo = 'a';
+	
+	while (lo <= 'z')
+	{
+		_putchar(lo);
+		lo = lo + 1;
+	}
+	_putchar('\n');
+	return (0);
+}
