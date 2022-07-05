cd8fb0f240e24a38b0efbce2e60faf88473ad257
MuhamaMark <muhamammark@gmail.com>
2022-07-05 16:04:45 -0700

    checking for abusolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - return absolute value of a number
+ * @n: parameter
+ * Return: absolute value of parameter
+ */
+int _abs(int)
+{
+	if (n < 0)
+	{
+
+		n = -n;
+	}
+	return (n);
+}
