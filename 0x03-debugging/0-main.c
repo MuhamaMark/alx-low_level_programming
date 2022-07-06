805ebe84f750664e211d81564e174e0926e9bfd7
MuhamaMark <muhamammark@gmail.com>
2022-07-06 10:29:29 -0700

    c dubugging task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/**
+ * main - tests function that prints intger is positive or negative
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
