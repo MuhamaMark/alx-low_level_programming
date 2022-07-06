8db1d7aa1edc02359ef26594edb134d126f0fc9f
MuhamaMark <muhamammark@gmail.com>
2022-07-05 18:00:59 -0700

    addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,9 +3,12 @@ include "main.h"
  * add -> adds two number
  * @x : param1
  * @y : param2
- * Return: x + y
+ * Return: z
  */
 int add(int x, int y)
 {
-	return (x + y);
+	int x, y, z;
+
+	z = x + y;
+	return (z);
 }
