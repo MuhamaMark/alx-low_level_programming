0b7b93cb018bb2022ab05b33bd8a7933cb87d2d6
MuhamaMark <muhamammark@gmail.com>
2022-07-07 16:55:12 -0700

    printing numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,8 +2,9 @@
 /**
  * print_numbers: prints the numbers from 0 to 9
  * followed by newline
+ * @n: passed as arg.
  */
-void print_numbers(void)
+void print_numbers(int n)
 {
 	int n = 0;
 	for (n = 0; n <= 9; n++)
