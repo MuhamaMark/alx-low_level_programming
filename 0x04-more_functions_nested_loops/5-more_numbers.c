96289e9239b9d1726558e7e17127df71641fd695
MuhamaMark <muhamammark@gmail.com>
2022-07-08 03:47:28 -0700

    counting 10 times from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers 0 to 14
- * 10 times and a newline.
+ * more_numbers - a function that prints 10 times the numbers, from 0 to 14,
+ * followed by a new line.
+ * Return: Always 0
  */
 void more_numbers(void)
 {
@@ -13,8 +14,10 @@ void more_numbers(void)
 		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
+			{
 				_putchar(num / 10 + '0');
-				_putchar(num % 10 + '0');
+			}
+			_putchar(num % 10 + '0');
 		}
 		_putchar('\n');
 	}
