3283e66a05b1f7a5f25c8a184d813a5be4541a0d
MuhamaMark <muhamammark@gmail.com>
2022-07-08 06:24:50 -0700

    Write a function that prints an integer.

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - Finds and prints the largest prime
  * 	factor of the number 612852475143.
  * Return: Always 0.
  */
 int main(void)
-{
+{	
 	long prime = 612852475143, div;
 
 	while (div < (prime / 2))
 	{
-		if ((prime % 2) ==0)
+		if ((prime % 2) == 0)
 		{
 			prime /= 2;
 			continue;
@@ -22,6 +23,7 @@ int main(void)
 				prime /= div;
 		}
 	}
+
 	printf("%ld\n", prime);
 	return (0);
 }
