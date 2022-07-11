7333dd2e08fead4c97733816f4731cd758f2fd5e
MuhamaMark <muhamammark@gmail.com>
2022-07-11 12:58:10 -0700

    a function that prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * _puts - a function that prints a string
+ * @str: being parameter
+ *
+ * Return: 0 success
+ */
+void _puts(char *str)
+{
+	_putchar(*str);
+	_putchar('\n');
+}
