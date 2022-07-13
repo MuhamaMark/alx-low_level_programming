cae69690850d6e0ef0ed2a280d9286fcf1f2c356
MuhamaMark <muhamammark@gmail.com>
2022-07-13 13:02:16 -0700

    function that changes all lowercase letters to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "maing.h"
+#include<ctype.h>
+#include<string.h>
+#include<stdio.h>
+/**
+ * string_toupper -> function that changes all lowercase letters to upper
+ * Return: 0 alwasy success
+ * @"": param1
+ */
+char *string_toupper(char *)
+{
+	char ch;
+
+	toupper(ch);
+	return (0);
+}
