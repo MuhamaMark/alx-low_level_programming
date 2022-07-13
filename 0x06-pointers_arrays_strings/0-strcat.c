9cade6a9463c84fe7b816f8a7ef906e523df3a06
MuhamaMark <muhamammark@gmail.com>
2022-07-13 09:54:43 -0700

    pointers, arrays and strings coding exercise

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+/**
+ * _strcat -> function tp concatnate two strings
+ * @dest: parameter for destination
+ * @src: parameter for source
+ */
+char *_strcat(char *dest, char *src)
+{
+	*ptr = strcat(*src, *dest);
+	dest = *ptr;
+
+	return (dest);
+}
