8295b69822c4465fe8ca0a477b135b685405c840
MuhamaMark <muhamammark@gmail.com>
2022-07-14 14:11:31 -0700

    rot 13 coding

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,5 +9,21 @@
  */
 char *rot13(char *p)
 {
-	p = rot13(s);
+	int a = o;
+
+	while (s[a])
+	{
+		while ((s[a] >= 'a' && s[a] <= 'z) || (s[a] >= 'A' && s[a] <= 'z'))
+		{
+			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
+			{
+				s[a] -= 13;
+				break;
+			}
+			s[a] += 13;
+			break;
+		}
+		a++;
+	}
+	return (s);
 }
