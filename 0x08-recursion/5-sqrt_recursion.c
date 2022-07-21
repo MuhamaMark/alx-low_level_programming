587f37275f522a8e08613fbfb4aa7c8fce232309
MuhamaMark <muhamammark@gmail.com>
2022-07-21 10:33:16 -0700

    hot fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,7 +4,8 @@
 
 int _sqrt(int, int);
 /**
- * _sqrt_recursion -> a function that returns the natural square root of a number.
+ * _sqrt_recursion -> a function that returns the natural square 
+ 		      root of a number.
  * @n: param1
  * Return: square root of natural number
  */
