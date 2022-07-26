7ac9310914a2fec98e8df3463624b4e39c04ec97
MuhamaMark <muhamammark@gmail.com>
2022-07-26 10:01:36 -0700

    hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,9 @@
  * argv: argc param
  * Return: 0 for success 1 for failure
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc __attribute__((__unused__)), char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
