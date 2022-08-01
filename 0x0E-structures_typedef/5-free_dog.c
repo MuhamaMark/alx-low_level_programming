375018ce254c34164ba6ece071f40b34f69a067a
muhama mark <muhamamark@gmail.com>
2022-08-01 20:58:54 +0300

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * free_dog - frees dogs.
+ * free_dog - frees memory.
  * @d: struct dog.
- *
+ * Description: free memory from struct
+ 
  * Return: no return.
  */
 void free_dog(dog_t *d)
